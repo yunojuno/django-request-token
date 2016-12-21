531a17cb850d550634dea34c33d16137b2cb7170
Hugo Rodger-Brown <hugo@yunojuno.com>
2016-12-21 16:12:37 +0000

    Bump version to v0.3.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="django-request-token",
-    version="0.2.3",
+    version="0.3.0",
     packages=find_packages(),
     install_requires=REQUIREMENTS,
     include_package_data=True,
