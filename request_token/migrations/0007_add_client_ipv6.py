b6adc020d329b7c86cd947402708c0190fc2d4c5
Hugo Rodger-Brown <hugo@yunojuno.com>
2016-12-22 17:45:07 +0000

    Fix migration in previous commit
    The varchar to inet column migration needs raw SQL.
    Bump version to v0.3.1
    Add sqlparse to requirements

--- request_token/migrations/0007_add_client_ipv6.py
+++ request_token/migrations/0007_add_client_ipv6.py
@@ -1,7 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.conf import settings
 from django.db import models, migrations
+
+# gets around "django.db.utils.ProgrammingError: column "client_ip" cannot be cast automatically to type inet"
+ALTER_SQL = (
+    "ALTER TABLE request_token_requesttokenlog "
+    "ALTER COLUMN client_ip TYPE inet "
+    "USING client_ip::inet;"
+)
+
+# if we are using postgresql then we use the ALTER_FIELD version
+POSTGRES = (settings.DATABASES['default']['ENGINE'] == 'django.db.backends.postgresql_psycopg2')
 
 
 class Migration(migrations.Migration):
@@ -10,10 +21,21 @@ class Migration(migrations.Migration):
         ('request_token', '0006_auto_20161104_1428'),
     ]
 
-    operations = [
-        migrations.AlterField(
-            model_name='requesttokenlog',
-            name='client_ip',
-            field=models.GenericIPAddressField(help_text=b'Client IP of device used to make the request.', null=True, unpack_ipv4=True, blank=True),
-        ),
-    ]
+    alter_field = migrations.AlterField(
+        model_name='requesttokenlog',
+        name='client_ip',
+        field=models.GenericIPAddressField(
+            help_text=b'Client IP of device used to make the request.',
+            null=True,
+            unpack_ipv4=True,
+            blank=True
+        )
+    )
+
+    run_sql = migrations.RunSQL(
+        ALTER_SQL,  # run the SQL to alter the column
+        None,       # reverse_sql - none available
+        state_operations=[alter_field]
+    )
+
+    operations = [run_sql] if POSTGRES else [alter_field]
