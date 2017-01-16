14ba006240698405e1860dcc0ce03e6a82be64f9
Darian Moody <mail@djm.org.uk>
2017-01-16 17:35:16 +0000

    Bump version for development

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="django-request-token",
-    version="0.3.3",
+    version="0.3.4-dev",
     packages=find_packages(),
     install_requires=REQUIREMENTS,
     include_package_data=True,
