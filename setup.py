bea81ef967ce86481059ccbd16570919f2ffe6b7
Hugo Rodger-Brown <hugo@yunojuno.com>
2017-03-06 12:39:17 +0000

    Bump version to 0.4 (Py3 support)

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="django-request-token",
-    version="0.3.4-dev",
+    version="0.4",
     packages=find_packages(),
     install_requires=REQUIREMENTS,
     include_package_data=True,
