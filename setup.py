2dddd927be45db8b02b7c59f3fac9e88140c20bc
Hugo Rodger-Brown <hugo@rodger-brown.com>
2017-05-02 17:06:50 +0100

    Add py36 to tox.ini, bump version to 0.5.2

--- setup.py
+++ setup.py
@@ -1,21 +1,22 @@
 # -*- coding: utf-8 -*-
-"""request_token package setup."""
 import os
 from setuptools import setup, find_packages
 
 README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
-# requirements.txt must be included in MANIFEST.in and include_package_data must be True
-# in order for this to work; ensures that tox can use the setup to enforce requirements
-REQUIREMENTS = '\n'.join(open(os.path.join(os.path.dirname(__file__), 'requirements.txt')).readlines())  # noqa
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="django-request-token",
-    version="0.5.2-dev",
+    version="0.5.2",
     packages=find_packages(),
-    install_requires=REQUIREMENTS,
+    install_requires=[
+        'Django>=1.9',
+        'PyJWT>=1.4',
+        'sqlparse>=0.2',
+        'psycopg2>=2.7'
+    ],
     include_package_data=True,
     description='JWT-backed Django app for managing querystring tokens.',
     license='MIT',
@@ -23,16 +24,20 @@ setup(
     url='https://github.com/yunojuno/django-request-token',
     author='YunoJuno',
     author_email='code@yunojuno.com',
-    maintainer='Hugo Rodger-Brown',
-    maintainer_email='hugo@yunojuno.com',
+    maintainer='YunoJuno',
+    maintainer_email='code@yunojuno.com',
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
+        'Framework :: Django :: 1.9',
+        'Framework :: Django :: 1.10',
+        'Framework :: Django :: 1.11',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
