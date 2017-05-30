e3e52e24152fdbe804362d06a89a6d3cfb50ffe2
Hugo Rodger-Brown <hugo@yunojuno.com>
2017-05-30 16:36:07 +0100

    Bump version to 0.7.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="django-request-token",
-    version="0.7",
+    version="0.7.1",
     packages=find_packages(),
     install_requires=[
         'Django>=1.9',
