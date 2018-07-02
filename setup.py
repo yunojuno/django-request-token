a3228d2149dd929acd0b973a0956ed91a2f993f1
Hugo Rodger-Brown <hugo@yunojuno.com>
2018-07-02 10:55:21 +0100

    Replace psycopg2 with psycopg2-binary
    As per warning:
    
    > UserWarning: The psycopg2 wheel package will be renamed from release 2.8; in order to keep installing from binary please use "pip install psycopg2-binary" instead.

--- setup.py
+++ setup.py
@@ -9,13 +9,13 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="django-request-token",
-    version="0.8",
+    version="0.8.1",
     packages=find_packages(),
     install_requires=[
         'Django>=1.11',
         'PyJWT>=1.4',
         'sqlparse>=0.2',
-        'psycopg2>=2.7'
+        'psycopg2-binary>=2.7'
     ],
     include_package_data=True,
     description='JWT-backed Django app for managing querystring tokens.',
